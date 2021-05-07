9097f535c17bd1e485e7e3a2f933edbd86cd2575
Grorruss <39708974+Grorruss@users.noreply.github.com>
2021-05-07 14:54:32 +0200

    Added PasswordValidator class with methods and the admin password

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class PasswordValidator {
+public class PasswordValidator {
+
+    public boolean validatePassword(String password, String type){
+        return lengthValidatePassword(password,  type) && numValidatePassword(password) && charValidatePassword(password);
+    }
+
+    public boolean lengthValidatePassword(String password, String type) {
+        boolean res = false;
+        if (type == "normal"){
+            if (password.length() >= 6){
+                res = true;
+            }
+        }
+        if (type == "admin"){
+            if (password.length() >= 10){
+                res = true;
+            }
+        }
+        return res;
+    }
+
+    public boolean charValidatePassword(String password) {
+        return password.matches(".*[a-zA-Z].*");
+    }
+
+    public boolean numValidatePassword(String password) {
+        return password.matches(".*[0-9].*");
+    }
+
+    public boolean adminPassword(String password){
+        return validatePassword(password, "admin") && specialCharPassword(password);
+    }
+
+    public boolean specialCharPassword(String password){
+        return password.matches(".*[^a-zA-Z0-9].*");
+    }
 }
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,4 +1,71 @@
-public class PasswordTest {
-    @Test
+import org.junit.jupiter.api.*;
 
+public class PasswordValidatorTest {
+
+    static PasswordValidator passwordValidator;
+
+    @BeforeAll
+    public static void initPasswordValidator(){
+        passwordValidator = new PasswordValidator();
+    }
+
+    @Test
+    public void smallLengthPasswordTest(){
+        Assertions.assertFalse(passwordValidator.lengthValidatePassword("abcd2", "normal"));
+    }
+
+    @Test
+    public void numberOnlyPasswordTest(){
+        Assertions.assertFalse(passwordValidator.charValidatePassword("123456"));
+    }
+
+    @Test
+    public void lettersOnlyPasswordTest(){
+        Assertions.assertFalse(passwordValidator.numValidatePassword("abcdef"));
+    }
+
+    @Test
+    public void lengthPasswordTest(){
+        Assertions.assertTrue(passwordValidator.lengthValidatePassword("abcdef2", "normal"));
+    }
+
+    @Test
+    public void numPasswordTest(){
+        Assertions.assertTrue(passwordValidator.numValidatePassword("abcde3"));
+    }
+
+    @Test
+    public void charPasswordTest(){
+        Assertions.assertTrue(passwordValidator.charValidatePassword("abcdef"));
+    }
+
+    @Test
+    public void specialCharPasswordTest(){
+        Assertions.assertTrue(passwordValidator.specialCharPassword("abcdef*"));
+    }
+
+    @Test
+    public void noSpecialCharPasswordTest(){
+        Assertions.assertFalse(passwordValidator.specialCharPassword("abcdef"));
+    }
+
+    @Test
+    public void validatePasswordTest(){
+        Assertions.assertTrue(passwordValidator.validatePassword("abcdef2", "normal"));
+    }
+
+    @Test
+    public void notValidatePasswordTest(){
+        Assertions.assertFalse(passwordValidator.validatePassword("abcd", "normal"));
+    }
+
+    @Test
+    public void adminPasswordTest(){
+        Assertions.assertTrue(passwordValidator.adminPassword("abcdefghijkl2*"));
+    }
+
+    @Test
+    public void notAdminPasswordTest(){
+        Assertions.assertFalse(passwordValidator.adminPassword("abcdef2*"));
+    }
 }
